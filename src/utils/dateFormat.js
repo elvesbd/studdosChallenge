0ec56aabda931763c7b24f077903dfbeef6e2835
elves brito <elvesbd@yahoo.com>
2021-05-20 20:06:34 -0300

    updated

--- src/utils/dateFormat.js
+++ src/utils/dateFormat.js
@@ -1,12 +1,11 @@
-import format from 'date-fns/format';
-import ptBR from 'date-fns/locale/pt-BR';
-
 export function DateFormat(props) {
-  const date = new Date(props),
+  /* const date = new Date(props),
   day = date.getDate().toString().padStart(2, '0'),
   month = (date.getMonth() + 1).toString().padStart(2, '0'),
   year = date.getFullYear()
 
-  return `${day}/${month}/${year}`;
+  return `${day}/${month}/${year}`; */
+  let date = new Date(props).toLocaleDateString();
+  new date.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+  console.log(date)
 };
-
